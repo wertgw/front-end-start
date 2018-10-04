e0befe8a5b1c42a926a4b9a016b21f874e11898f
wertgw <wertgw@naver.com>
2018-10-04 17:19:46 +0900

    사진올리기

---
+++ 03/app1.js
@@ -0,0 +1,11 @@
+function test(){
+    console.log("test()");
+  }
+  // var id = setTimeout(test, 1000);
+  var id = setInterval(test, 1000);
+  
+  // app2.js  불러오기
+  var head = document.querySelector('head');
+  var script = document.createElement('script');
+  script.src = 'app2.js';
+  head.appendChild(script);
