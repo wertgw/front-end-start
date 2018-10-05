cbeb676b5058930d20357eda711ab1a0680bd9fb
wertgw <wertgw@naver.com>
2018-10-05 18:31:35 +0900

    More보기

--- 05/1boon/app.js
+++ 05/1boon/app.js
@@ -1,26 +1,28 @@
 console.log('1boon');
-
-var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=10'
-
-
-getUrlData(url, function(json){
-    console.log(json);
-
-    var str = '';
+var count = 1;
 
 
-    for(var i=0; i < json.data.length; i++){
+function aaaa() {
+    getUrlData(function(json){
+        console.log(json);
+    
+        var str = '';
+    
+        for(var i=0; i < json.data.length; i++){
+    
+            var title = json.data[i].title;
+            var path = json.data[i].path;
+            str += '<a href="https://1boon.kakao.com/'+path+'" target ="_blank">' + title + '</a><br>';
+        }
+    
+        document.getElementById('wrap').innerHTML += str;
+        count++;
+    })
+}
 
-        var title = json.data[i].title;
-        var path = json.data[i].path;
-        str += '<a href="https://1boon.kakao.com/'+path+'" target ="_blank">' + title + '</a><br>';
-    }
-
-
-    document.getElementById('wrap').innerHTML = str;
-})
-
-function getUrlData(url, callback) {
+function getUrlData(callback) {
+    var url = 'https://1boon.kakao.com/ch/enter.json?page=' + count + '&pagesize=10';
+    console.log(url);
     fetch(url)
     .then(function(response){
         response.json().then(function(data){
@@ -32,3 +34,7 @@ function getUrlData(url, callback) {
     console.log('Fetch Error : -S', err);
 });
 }
+var btn = document.querySelector("button");
+btn.addEventListener("click", aaaa);
+
+aaaa();
