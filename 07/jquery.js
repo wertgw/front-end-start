c3200a7259f82b3c52090cde290a932990c88582
wertgw <wertgw@naver.com>
2018-10-12 18:21:47 +0900

    스크롤

---
+++ 07/jquery.js
@@ -0,0 +1,41 @@
+
+//document.querySelector('h1').innerHTML= 'hi';
+
+//$('h1').fadeOut().delay(1000).fadeIn();
+
+
+//var $div = $('div')
+
+//$div
+//.css('background-color', 'yellow')
+//.css('border-color', 'red')
+
+ //var divs = document.querySelectorAll('div');
+// for(var i=0; i< divs.length; i++){
+  //   divs[i].style.backgroundColor = 'red';
+ //   }
+var $btn = $('#btn');
+var $txt = $('#txt');
+var $debug = $('#debug');
+
+$( window ).scroll(function() {
+    $( "span" ).css( "display", "inline" ).fadeOut( "slow" );
+  });
+
+$btn.on('click', function(event){
+console.log('click')
+
+$('h1').toggle('fast');//fadeOut();
+
+//if($txt.val() === ''){
+//    alert('값을 넣어주세요')
+//    return;
+//}
+//$debug.html($txt.val());
+});
+
+$('.wrap').on('click', 'div', function(event){
+    $(event.target).fadeOut();
+    //console.log(event.target);
+
+})
